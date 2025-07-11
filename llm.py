49e5ddf0adb6909f459a39147ea3c5b116ce5d84
akshithputta <akshith.putta@uta.edu>
2025-07-10 20:32:47 -0500

    initialize vllm

---
+++ llm.py
@@ -0,0 +1,28 @@
+import openai
+
+def prompt_vllm(prompt, model='Qwen/Qwen3-8B', think=True):
+    client = openai.OpenAI(
+        api_key='EMPTY',
+        base_url='http://localhost:8000/v1',  # Adjust the base URL if needed
+    )
+
+    messages = []
+    if not think:
+        messages.append({'role': 'system', 'content': '/no_think'})
+    messages.append({
+        'role': 'user',
+        'content': prompt,
+    })
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages
+    )
+
+    output = response.choices[0].message.content
+
+    if '<think>' in output[:20]:
+        output = output.split('</think>')[-1]
+    
+    return output
+ 
